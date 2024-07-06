a3e6f0cc633c0a35ffadcdd0a8227a3f4178e4fc
Moddy-t <amalembamodesta@gmail.com>
2024-07-06 06:49:53 +0300

    filter lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, query) {
+    const filteredDrivers = drivers.filter(driver =>
+      driver.toLowerCase().startsWith(query.toLowerCase())
+    );
+    return filteredDrivers;
+  }
+
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  
